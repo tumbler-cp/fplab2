a9e247eee23d3e476d8269b6752a706485dccedd
arahnik <abdujalol04@mail.ru>
2025-02-13 01:10:13 +0300

    formatting

--- Src/Program.fs
+++ Src/Program.fs
@@ -2,29 +2,38 @@
 
 module PreDict =
 
-    type TrieNode<'T> = {
-        Value: 'T option
-        Children: Map<char, TrieNode<'T>>
-        IsTerminal: bool
-    }
+    type TrieNode<'T> =
+        { Value: 'T option
+          Children: Map<char, TrieNode<'T>>
+          IsTerminal: bool }
 
-    type Trie<'T> = {
-        Root: TrieNode<'T>
-    }
+    type Trie<'T> = { Root: TrieNode<'T> }
 
     let insert (key: string) (value: 'T) (trie: Trie<'T>) : Trie<'T> =
         let rec insertHelper (node: TrieNode<'T>) (key: string) (index: int) : TrieNode<'T> =
             match index = key.Length with
-            | true -> { node with Value = Some value; IsTerminal = true }
+            | true ->
+                { node with
+                    Value = Some value
+                    IsTerminal = true }
             | false ->
                 let char = key.[index]
+
                 let childNode =
                     match Map.tryFind char node.Children with
                     | Some n -> n
-                    | None -> { Value = None; Children = Map.empty; IsTerminal = false }
+                    | None ->
+                        { Value = None
+                          Children = Map.empty
+                          IsTerminal = false }
+
                 let updatedChild = insertHelper childNode key (index + 1)
-                { node with Children = Map.add char updatedChild node.Children }
-        { trie with Root = insertHelper trie.Root key 0 }
+
+                { node with
+                    Children = Map.add char updatedChild node.Children }
+
+        { trie with
+            Root = insertHelper trie.Root key 0 }
 
     let remove (key: string) (trie: Trie<'T>) : Trie<'T> =
         let rec removeHelper (node: TrieNode<'T>) (key: string) (index: int) : TrieNode<'T> option =
@@ -32,25 +41,43 @@ module PreDict =
             | true ->
                 match node.IsTerminal with
                 | true ->
-                    let updatedNode = { node with Value = None; IsTerminal = false }
-                    if updatedNode.Children.IsEmpty then None else Some updatedNode
+                    let updatedNode =
+                        { node with
+                            Value = None
+                            IsTerminal = false }
+
+                    if updatedNode.Children.IsEmpty then
+                        None
+                    else
+                        Some updatedNode
                 | false -> Some node
             | false ->
                 let char = key.[index]
+
                 match Map.tryFind char node.Children with
                 | Some childNode ->
                     match removeHelper childNode key (index + 1) with
                     | Some updatedChild ->
-                        Some { node with Children = Map.add char updatedChild node.Children }
+                        Some
+                            { node with
+                                Children = Map.add char updatedChild node.Children }
                     | None ->
                         let updatedChildren = Map.remove char node.Children
+
                         if node.IsTerminal || not updatedChildren.IsEmpty then
                             Some { node with Children = updatedChildren }
-                        else None
+                        else
+                            None
                 | None -> Some node
+
         match removeHelper trie.Root key 0 with
         | Some updatedRoot -> { trie with Root = updatedRoot }
-        | None -> { trie with Root = { Value = None; Children = Map.empty; IsTerminal = false } }
+        | None ->
+            { trie with
+                Root =
+                    { Value = None
+                      Children = Map.empty
+                      IsTerminal = false } }
 
     let filter (predicate: 'T -> bool) (trie: Trie<'T>) : Trie<'T> =
         let rec filterHelper (node: TrieNode<'T>) : TrieNode<'T> option =
@@ -59,34 +86,52 @@ module PreDict =
                 |> Map.toSeq
                 |> Seq.choose (fun (char, childNode) ->
                     match filterHelper childNode with
-                    | Some filteredChild -> Some (char, filteredChild)
+                    | Some filteredChild -> Some(char, filteredChild)
                     | None -> None)
                 |> Map.ofSeq
+
             let isTerminal = node.IsTerminal && Option.exists predicate node.Value
+
             if isTerminal || not filteredChildren.IsEmpty then
-                Some { node with Children = filteredChildren; IsTerminal = isTerminal }
-            else None
+                Some
+                    { node with
+                        Children = filteredChildren
+                        IsTerminal = isTerminal }
+            else
+                None
+
         match filterHelper trie.Root with
         | Some filteredRoot -> { trie with Root = filteredRoot }
-        | None -> { trie with Root = { Value = None; Children = Map.empty; IsTerminal = false } }
+        | None ->
+            { trie with
+                Root =
+                    { Value = None
+                      Children = Map.empty
+                      IsTerminal = false } }
 
     let map (transform: 'T -> 'U) (trie: Trie<'T>) : Trie<'U> =
         let rec mapHelper (node: TrieNode<'T>) : TrieNode<'U> =
             let mappedValue = Option.map transform node.Value
+
             let mappedChildren =
-                node.Children
-                |> Map.map (fun _ childNode -> mapHelper childNode)
-            { Value = mappedValue; Children = mappedChildren; IsTerminal = node.IsTerminal }
+                node.Children |> Map.map (fun _ childNode -> mapHelper childNode)
+
+            { Value = mappedValue
+              Children = mappedChildren
+              IsTerminal = node.IsTerminal }
+
         { Root = mapHelper trie.Root }
 
     let fold (folder: 'State -> 'T -> 'State) (state: 'State) (trie: Trie<'T>) : 'State =
         let rec foldHelper (state: 'State) (node: TrieNode<'T>) : 'State =
-            let stateWithNode = 
+            let stateWithNode =
                 match node.Value with
                 | Some value when node.IsTerminal -> folder state value
                 | _ -> state
+
             node.Children
             |> Map.fold (fun acc _ childNode -> foldHelper acc childNode) stateWithNode
+
         foldHelper state trie.Root
 
     let foldBack (folder: 'T -> 'State -> 'State) (state: 'State) (trie: Trie<'T>) : 'State =
@@ -94,59 +139,70 @@ module PreDict =
             let stateWithChildren =
                 node.Children
                 |> Map.fold (fun acc _ childNode -> foldBackHelper acc childNode) state
+
             match node.Value with
             | Some value when node.IsTerminal -> folder value stateWithChildren
             | _ -> stateWithChildren
+
         foldBackHelper state trie.Root
 
-    let empty : Trie<'T> = 
-        { Root = { Value = None; Children = Map.empty; IsTerminal = false } }
+    let empty: Trie<'T> =
+        { Root =
+            { Value = None
+              Children = Map.empty
+              IsTerminal = false } }
 
     let merge (trie1: Trie<'T>) (trie2: Trie<'T>) : Trie<'T> =
         let rec mergeNodes (node1: TrieNode<'T>) (node2: TrieNode<'T>) : TrieNode<'T> =
             let mergedValue =
                 match node1.Value, node2.Value with
-                | Some v1, Some v2 when node1.IsTerminal && node2.IsTerminal -> Some v1 
+                | Some v1, Some v2 when node1.IsTerminal && node2.IsTerminal -> Some v1
                 | Some v, _ when node1.IsTerminal -> Some v
                 | _, Some v when node2.IsTerminal -> Some v
                 | _ -> None
+
             let mergedChildren =
                 node1.Children
-                |> Map.fold (fun acc key childNode1 ->
-                    match Map.tryFind key node2.Children with
-                    | Some childNode2 -> Map.add key (mergeNodes childNode1 childNode2) acc
-                    | None -> Map.add key childNode1 acc) node2.Children
-            { Value = mergedValue; Children = mergedChildren; IsTerminal = node1.IsTerminal || node2.IsTerminal }
+                |> Map.fold
+                    (fun acc key childNode1 ->
+                        match Map.tryFind key node2.Children with
+                        | Some childNode2 -> Map.add key (mergeNodes childNode1 childNode2) acc
+                        | None -> Map.add key childNode1 acc)
+                    node2.Children
+
+            { Value = mergedValue
+              Children = mergedChildren
+              IsTerminal = node1.IsTerminal || node2.IsTerminal }
+
         { Root = mergeNodes trie1.Root trie2.Root }
 
     let rec compareNodes (node1: TrieNode<'T>) (node2: TrieNode<'T>) : bool =
-        node1.Value = node2.Value &&
-        node1.IsTerminal = node2.IsTerminal &&
-        Map.forall (fun key child1 ->
-            match Map.tryFind key node2.Children with
-            | Some child2 -> compareNodes child1 child2
-            | None -> false
-        ) node1.Children &&
-        Map.forall (fun key _ -> Map.containsKey key node1.Children) node2.Children
+        node1.Value = node2.Value
+        && node1.IsTerminal = node2.IsTerminal
+        && Map.forall
+            (fun key child1 ->
+                match Map.tryFind key node2.Children with
+                | Some child2 -> compareNodes child1 child2
+                | None -> false)
+            node1.Children
+        && Map.forall (fun key _ -> Map.containsKey key node1.Children) node2.Children
 
-    let compareTrees (tree1: Trie<'T>) (tree2: Trie<'T>) : bool =
-        compareNodes tree1.Root tree2.Root
+    let compareTrees (tree1: Trie<'T>) (tree2: Trie<'T>) : bool = compareNodes tree1.Root tree2.Root
 
-    // let rec printTrieNode (node: TrieNode<'T>) (label: string) (indent: string) (isLast: bool) (printValue: 'T -> string) : unit =
-    //     let nodeLabel = if label = "" then "(root)" else label
-    //     let branch = if indent = "" then "" else if isLast then "└── " else "├── "
-    //     let terminalInfo =
-    //         if node.IsTerminal then sprintf " -> %s" (node.Value |> Option.map printValue |> Option.defaultValue "None")
-    //         else ""
-    //     printfn "%s%s%s%s" indent branch nodeLabel terminalInfo
-    //     let newIndent = indent + (if isLast then "    " else "│   ")
-    //     let children = node.Children |> Map.toList
-    //     let count = List.length children
-    //     children |> List.iteri (fun i (key, childNode) ->
-    //          let isLastChild = i = (count - 1)
-    //          printTrieNode childNode (string key) newIndent isLastChild printValue
-    //     )
-    
-    // let printTrie (trie: Trie<'T>) (printValue: 'T -> string) : unit =
-    //     printTrieNode trie.Root "" "" true printValue
+// let rec printTrieNode (node: TrieNode<'T>) (label: string) (indent: string) (isLast: bool) (printValue: 'T -> string) : unit =
+//     let nodeLabel = if label = "" then "(root)" else label
+//     let branch = if indent = "" then "" else if isLast then "└── " else "├── "
+//     let terminalInfo =
+//         if node.IsTerminal then sprintf " -> %s" (node.Value |> Option.map printValue |> Option.defaultValue "None")
+//         else ""
+//     printfn "%s%s%s%s" indent branch nodeLabel terminalInfo
+//     let newIndent = indent + (if isLast then "    " else "│   ")
+//     let children = node.Children |> Map.toList
+//     let count = List.length children
+//     children |> List.iteri (fun i (key, childNode) ->
+//          let isLastChild = i = (count - 1)
+//          printTrieNode childNode (string key) newIndent isLastChild printValue
+//     )
 
+// let printTrie (trie: Trie<'T>) (printValue: 'T -> string) : unit =
+//     printTrieNode trie.Root "" "" true printValue
--- Test/UnitTest1.fs
+++ Test/UnitTest1.fs
@@ -4,8 +4,7 @@ open NUnit.Framework
 open PrefixTreeDictionary.PreDict
 
 [<SetUp>]
-let Setup () =
-    ()
+let Setup () = ()
 
 let find key trie =
     let rec findHelper (node: TrieNode<'TValue>) (key: string) (index: int) =
@@ -15,11 +14,12 @@ let find key trie =
             match Map.tryFind key.[index] node.Children with
             | Some childNode -> findHelper childNode key (index + 1)
             | None -> None
+
     findHelper trie.Root key 0
 
 [<Test>]
 let ``Insert and Find Test`` () =
-    let trie = 
+    let trie =
         empty
         |> insert "hello" 1
         |> insert "hell" 2
@@ -27,16 +27,16 @@ let ``Insert and Find Test`` () =
         |> insert "hero" 4
         |> insert "her" 5
 
-    Assert.That(find "hello" trie, Is.EqualTo (Some 1))
-    Assert.That(find "hell" trie, Is.EqualTo (Some 2))
-    Assert.That(find "he" trie, Is.EqualTo (Some 3))
-    Assert.That(find "hero" trie, Is.EqualTo (Some 4))
-    Assert.That(find "her" trie, Is.EqualTo (Some 5))
+    Assert.That(find "hello" trie, Is.EqualTo(Some 1))
+    Assert.That(find "hell" trie, Is.EqualTo(Some 2))
+    Assert.That(find "he" trie, Is.EqualTo(Some 3))
+    Assert.That(find "hero" trie, Is.EqualTo(Some 4))
+    Assert.That(find "her" trie, Is.EqualTo(Some 5))
     Assert.That(find "hi" trie, Is.EqualTo None)
 
 [<Test>]
 let ``Remove Test`` () =
-    let trie = 
+    let trie =
         empty
         |> insert "hello" 1
         |> insert "hell" 2
@@ -46,90 +46,88 @@ let ``Remove Test`` () =
         |> remove "hell"
         |> remove "hero"
 
-    Assert.That(find "hello" trie, Is.EqualTo (Some 1))
+    Assert.That(find "hello" trie, Is.EqualTo(Some 1))
     Assert.That(find "hell" trie, Is.EqualTo None)
-    Assert.That(find "he" trie, Is.EqualTo (Some 3))
+    Assert.That(find "he" trie, Is.EqualTo(Some 3))
     Assert.That(find "hero" trie, Is.EqualTo None)
-    Assert.That(find "her" trie, Is.EqualTo (Some 5))
+    Assert.That(find "her" trie, Is.EqualTo(Some 5))
 
 [<Test>]
 let ``Fold Test`` () =
-    let trie = 
+    let trie =
         empty
         |> insert "hello" 1
         |> insert "hell" 2
         |> insert "he" 3
         |> insert "hero" 4
         |> insert "her" 5
+
     let sumFolder acc value = acc + value
     let sum = fold sumFolder 0 trie
     Assert.That(sum, Is.EqualTo 15)
 
 [<Test>]
 let ``FoldBack Test`` () =
-    let trie = 
+    let trie =
         empty
         |> insert "hello" 1
         |> insert "hell" 2
         |> insert "he" 3
         |> insert "hero" 4
         |> insert "her" 5
+
     let sumFolder value acc = acc + value
     let sum = foldBack sumFolder 0 trie
     Assert.That(sum, Is.EqualTo 15)
 
 [<Test>]
 let ``Filter Test`` () =
-    let trie = 
+    let trie =
         empty
         |> insert "hello" 1
         |> insert "hell" 2
         |> insert "he" 3
         |> insert "hero" 4
         |> insert "her" 5
+
     let isEven value = value % 2 = 0
     let filteredTrie = filter isEven trie
     Assert.That(find "hello" filteredTrie, Is.EqualTo None)
-    Assert.That(find "hell" filteredTrie, Is.EqualTo (Some 2))
+    Assert.That(find "hell" filteredTrie, Is.EqualTo(Some 2))
     Assert.That(find "he" filteredTrie, Is.EqualTo None)
-    Assert.That(find "hero" filteredTrie, Is.EqualTo (Some 4))
+    Assert.That(find "hero" filteredTrie, Is.EqualTo(Some 4))
     Assert.That(find "her" filteredTrie, Is.EqualTo None)
 
 [<Test>]
 let ``Map Test`` () =
-    let trie = 
+    let trie =
         empty
         |> insert "hello" 1
         |> insert "hell" 2
         |> insert "he" 3
         |> insert "hero" 4
         |> insert "her" 5
+
     let double value = value * 2
     let mappedTrie = map double trie
-    Assert.That(find "hello" mappedTrie, Is.EqualTo (Some 2))
-    Assert.That(find "hell" mappedTrie, Is.EqualTo (Some 4))
-    Assert.That(find "he" mappedTrie, Is.EqualTo (Some 6))
-    Assert.That(find "hero" mappedTrie, Is.EqualTo (Some 8))
-    Assert.That(find "her" mappedTrie, Is.EqualTo (Some 10))
+    Assert.That(find "hello" mappedTrie, Is.EqualTo(Some 2))
+    Assert.That(find "hell" mappedTrie, Is.EqualTo(Some 4))
+    Assert.That(find "he" mappedTrie, Is.EqualTo(Some 6))
+    Assert.That(find "hero" mappedTrie, Is.EqualTo(Some 8))
+    Assert.That(find "her" mappedTrie, Is.EqualTo(Some 10))
 
 [<Test>]
 let ``Monoid Test`` () =
-    let trie1 = 
-        empty
-        |> insert "hello" 1
-        |> insert "hell" 2
+    let trie1 = empty |> insert "hello" 1 |> insert "hell" 2
 
-    let trie2 = 
-        empty
-        |> insert "hero" 3
-        |> insert "her" 4
+    let trie2 = empty |> insert "hero" 3 |> insert "her" 4
 
     let mergedTrie = merge trie1 trie2
 
-    Assert.That(find "hello" mergedTrie, Is.EqualTo (Some 1))
-    Assert.That(find "hell" mergedTrie, Is.EqualTo (Some 2))
-    Assert.That(find "hero" mergedTrie, Is.EqualTo (Some 3))
-    Assert.That(find "her" mergedTrie, Is.EqualTo (Some 4))
+    Assert.That(find "hello" mergedTrie, Is.EqualTo(Some 1))
+    Assert.That(find "hell" mergedTrie, Is.EqualTo(Some 2))
+    Assert.That(find "hero" mergedTrie, Is.EqualTo(Some 3))
+    Assert.That(find "her" mergedTrie, Is.EqualTo(Some 4))
 
     let emptyTrie = empty
     let mergedWithEmpty = merge mergedTrie emptyTrie
